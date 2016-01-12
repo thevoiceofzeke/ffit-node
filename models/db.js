ab7dd44c3d5c031062cc688247bc1d192d8d30a7
zekeWitter <david.witter@wisc.edu>
2016-01-12 11:18:19 -0600

    Add MongoLab URI

--- models/db.js
+++ models/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ffit-node-db');
+// mongoose.connect('mongodb://localhost/ffit-node-db'); // LOCAL
+mongoose.connect('mongodb://heroku_gmzqnpc4:46de1t0k63q35ke27lan8aohn3@ds043605.mongolab.com:43605/heroku_gmzqnpc4'); // MongoLab DB
